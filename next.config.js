db6684b1d8b98aad92030fe2a1240ff66718c977
dev-yoonjung <dear__yoon@naver.com>
2022-07-02 13:10:13 +0900

    fix: fix styled-component flash of unstyled content issue using swc

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  swcMinify: true,
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
 }
 
 module.exports = nextConfig
